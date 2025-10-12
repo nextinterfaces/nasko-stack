9a1b39f08e57dd6f4432aa0e68e34c93ce8fae1d
Atanas Roussev <atanas@nextinterfaces.com>
2025-10-12 15:52:40 -0700

    fix duplciate

--- infra/hetzner-microk8s/main.tf
+++ infra/hetzner-microk8s/main.tf
@@ -52,12 +52,3 @@ resource "hcloud_firewall" "k8s" {
     source_ips = ["0.0.0.0/0", "::/0"]
   }
 }
-
-output "server_ip" {
-  value = hcloud_server.microk8s.ipv4_address
-}
-
-output "access_url" {
-  value = "https://${hcloud_server.microk8s.ipv4_address}.nip.io/test"
-}
-
